96850a703a33754a9f206dc2882b9c509c6d156d
Jakob Hendrix <jhendrix001@gmail.com>
2019-12-19 15:48:31 -0600

    12.6 done - abstracted away some the actions

--- RpgTutorial/Engine/Actions/AttackWithWeapon.cs
+++ RpgTutorial/Engine/Actions/AttackWithWeapon.cs
@@ -3,19 +3,17 @@ using Engine.Models;
 
 namespace Engine.Actions
 {
-    public class AttackWithWeapon : IAction
+    public class AttackWithWeapon : BaseAction, IAction
     {
-        private readonly GameItem _weapon;
         private readonly int _minimumDamage;
         private readonly int _maximumDamage;
 
-        public event EventHandler<string> OnActionPerformed;
-
-        public AttackWithWeapon(GameItem weapon, int minimumDamage, int maximumDamage)
+        public AttackWithWeapon(GameItem itemInUse, int minimumDamage, int maximumDamage)
+            : base(itemInUse)
         {
-            if (weapon.Category != GameItem.ItemCategory.Weapon)
+            if (itemInUse.Category != GameItem.ItemCategory.Weapon)
             {
-                throw new ArgumentException($"{weapon.Name} is not a weapon");
+                throw new ArgumentException($"{itemInUse.Name} is not a itemInUse");
             }
 
             if (minimumDamage < 0)
@@ -28,7 +26,6 @@ namespace Engine.Actions
                 throw new ArgumentException("Maximum cannot be less than the minimum damage");
             }
 
-            _weapon = weapon;
             _minimumDamage = minimumDamage;
             _maximumDamage = maximumDamage;
         }
@@ -50,7 +47,5 @@ namespace Engine.Actions
                 target.TakeDamage(damage);
             }
         }
-
-        private void ReportResult(string result) => OnActionPerformed?.Invoke(this, result);
     }
 }
